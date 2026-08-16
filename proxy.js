28e1a4198bb1b412fb5a548e8b02501b53170a84
DROPNET <bielsshdropnet@gmail.com>
2026-08-16 19:21:02 -0300

    Update proxy.js

--- proxy.js
+++ proxy.js
@@ -2,7 +2,7 @@ export const config = {
   runtime: "edge",
 };
 
-const VPS_TARGET = "https://ativo.dpnet.online:80";
+const VPS_TARGET = "https://bunny.dpnet.online:443";
 
 export default async function handler(req) {
   const url = new URL(req.url);
