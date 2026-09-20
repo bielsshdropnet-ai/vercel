ec00485c902be692d4fe2234b4776539a4d5aaf4
DROPNET <bielsshdropnet@gmail.com>
2026-09-19 22:15:17 -0300

    Change VPS target URL from HTTPS to HTTP

--- proxy.js
+++ proxy.js
@@ -2,7 +2,7 @@ export const config = {
   runtime: "edge",
 };
 
-const VPS_TARGET = "https://bunny.dpnet.online:443";
+const VPS_TARGET = "http://biel.dpnet.online:80";
 
 export default async function handler(req) {
   const url = new URL(req.url);
